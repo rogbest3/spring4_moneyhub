0d8c798cbb519ccebdb133ae76449984ed9351e3
rogbest3 <rogbest3@gmail.com>
2019-10-10 17:09:00 +0900

    191010

---
+++ src/main/java/com/hr/web/command/Receiver.java
@@ -0,0 +1,11 @@
+package com.hr.web.command;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Receiver {
+	public static Command cmd = new Command();
+	public static void init(HttpServletRequest request) {
+		cmd = Commander.direct(request);
+	}
+
+}
---
+++ src/main/java/com/hr/web/pool/Constants.java
@@ -0,0 +1,10 @@
+package com.hr.web.pool;
+
+public class Constants {
+	public static final String DOUBLE_PATH = "/WEB-INF/views/%s/%s.jsp",
+	//		USERNAME ="c##erin", 
+	//		PASSWORD ="890720",
+			USERNAME ="c##kmk", 
+			PASSWORD ="4683",
+			VENDOR = "ORACLE";	
+}
